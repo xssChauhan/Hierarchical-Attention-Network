b8d5563880e4ec276ac9bc7e4789b82b6896d958
Shikhar Chauhan <shikhar.chauhan@live.com>
2019-02-28 13:36:01 +0530

    Moving models to a separate file

--- models.py
+++ models.py
@@ -6,13 +6,14 @@ from torch.autograd import Variable
 
 
 class WordGRU(nn.Module):
+
     def __init__(
         self,
         embedding_dim,
         vocab_size,
         batch_size=1,
         hidden_size=100,
-        bidirectional=False,
+        bidirectional=True,
         cuda=True
     ):
 
@@ -78,6 +79,10 @@ class WordGRU(nn.Module):
 class WordAttention(nn.Module):
 
     def __init__(self, hidden_size):
+        """
+
+        :param hidden_size: Number of features in the incoming word vecs
+        """
 
         super().__init__()
 
@@ -104,9 +109,9 @@ class SentenceGRU(nn.Module):
     def __init__(self, input_size, hidden_size, bidirectional=True):
         """
 
-        :param input_size:
-        :param hidden_size:
-        :param bidirectional:
+        :param input_size: Size of incoming sentence vecs
+        :param hidden_size: Hidden size of GRU unit
+        :param bidirectional: If the GRU should be bidirectional
         """
         super().__init__()
 
@@ -121,15 +126,18 @@ class SentenceGRU(nn.Module):
     def forward(self, sentence_outputs, hidden):
         """
 
-        :param sentence_outputs:
+        :param sentence_outputs: Sentence vecs from the word attention layer
         :return:
         """
-        return self.gru( sentence_outputs, hidden)
+        return self.gru(sentence_outputs, hidden)
 
 
 class SentenceAttention(nn.Module):
 
     def __init__(self, input_size):
+        """
+        :param input_size: Size of vectors from the sentence GRU
+        """
 
         super().__init__()
 
@@ -138,6 +146,11 @@ class SentenceAttention(nn.Module):
         self.sentence_context = nn.Parameter(torch.randn(input_size,1))
 
     def forward(self, sent_outputs):
+        """
+
+        :param sent_outputs: Sentence vectors from sentence GRU
+        :return:
+        """
 
         o = self.linear(sent_outputs)
         o = self.activation(o)
@@ -146,3 +159,97 @@ class SentenceAttention(nn.Module):
         o = torch.sum(o, dim=1)
 
         return o
+
+
+class OutputLayer(nn.Module):
+
+    def __init__(self, input_size, num_labels):
+        """
+
+        :param input_size: Number of features in the incoming vector
+        :param num_labels: Number of labels in the data
+        """
+
+        super().__init__()
+        self.input_size = input_size
+        self.num_labels = num_labels
+
+        self.linear = nn.Linear(input_size, num_labels)
+        self.softmax = nn.LogSoftmax()
+
+    def forward(self, doc_vector):
+        """
+
+        :param doc_vector: Document Vector
+        :return:
+        """
+
+        o = self.linear(doc_vector)
+        o = self.softmax(o)
+
+        return o
+
+
+class HAN(nn.Module):
+
+    def __init__(self, vocab_size, embedding_dim ,word_hidden_size, sent_hidden_size, num_labels, batch_size, bidirectional, cuda):
+        """
+
+        :param vocab_size:
+        :param word_hidden_size:
+        :param sent_hidden_size:
+        :param num_labels:
+        :param batch_size:
+        :param bidirectional:
+        :param cuda:
+        """
+
+        self.vocab_size = vocab_size
+        self.embedding_dim = embedding_dim
+        self.word_hidden_size = word_hidden_size
+        self.sent_hidden_size = sent_hidden_size
+        self.num_labels = num_labels
+        self.batch_size = batch_size
+        self.bidirectional = bidirectional
+        self.cuda = cuda
+
+        self.directions = 2 if bidirectional else 1
+
+        self.word_gru = WordGRU(
+            embedding_dim, vocab_size, word_hidden_size, bidirectional, cuda
+        )
+        self.word_attn = WordAttention(word_hidden_size * directions)
+
+        self.sentence_gru = SentenceGRU(
+            word_hidden_size * directions, sent_hidden_size, bidirectional
+        )
+        self.sentence_attn = SentenceAttention(
+            directions * sent_hidden_size
+        )
+
+        self.output_layer = OutputLayer(
+            directions * sent_hidden_size
+        )
+
+
+        def forward(self, inputs):
+            """
+
+            :param self:
+            :param inputs:
+            :return:
+            """
+            # Get encoded sentences
+            # Unsqueeze them
+            # Combine them
+            return
+
+        def set_embedding(self, embedding):
+            """
+            Initialize the Embedding
+
+            :param self:
+            :param embedding:
+            :return:
+            """
+            return
